e618be4e638a325d8e002436d49d7b8b9f8ebcea
Said <slaoualy@yahoo.fr>
2023-09-09 19:02:33 +0200

    Added 5 and 25 divs

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,20 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className="container h-screen bg-slate-100 flex items-center justify-items-center">
+      <div className="container h-screen bg-slate-100 flex flex-col items-center justify-center">
         <div>
           <h1>25 + 5 Clock</h1>
         </div>
-        <div id="break-label">Break length</div>
-        <div id="session-label">Session Length</div>
+        <div className="flex flex-row gap-5">
+          <div>
+            <div id="break-label">Break length</div>
+            <div>UP 5 DOWN</div>
+          </div>
+          <div>
+            <div id="session-label">Session Length</div>
+            <div>UP 25 DOWN</div>
+          </div>
+        </div>
       </div>
     </>
   );
