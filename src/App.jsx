9baac2d67649ede051d925b64c7e844215dc3866
Said <slaoualy@yahoo.fr>
2023-09-09 18:55:36 +0200

    Added break-label

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container h-screen bg-slate-100 flex items-center justify-items-center">
+        <div>
+          <h1>25 + 5 Clock</h1>
+        </div>
+        <div id="break-label">Break length</div>
+        <div>Session Length</div>
+      </div>
+    </>
+  );
 }
 
 export default App;
