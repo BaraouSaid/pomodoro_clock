134d77935658a74197b1cb7bee36fb0d323a1653
Said <slaoualy@yahoo.fr>
2023-09-10 21:34:23 +0200

    imported play pause repeat from bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
+import { BsFillPlayFill, BsPauseFill, BsArrowRepeat } from 'react-icons/bs';
 import './App.css';
 
 function App() {
@@ -29,10 +30,11 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="p-3 border-yellow-600 rounded-xl">
+        <div className="p-3 border-4 border-yellow-600 rounded-xl">
           <h2 id="timer-label">Session</h2>
           <p id="time-left">25 : 00</p>
         </div>
+        <div className="flex items-center gap-2"></div>
       </div>
     </>
   );
