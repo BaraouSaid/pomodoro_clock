f3931f13478b3384023456cfca807ac07b0e2d12
Said <slaoualy@yahoo.fr>
2023-09-10 21:19:41 +0200

    Added arrows Added ids of break and session

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import { FaArrowUp } from 'react-icons/fa';
+import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
 import './App.css';
 
 function App() {
   return (
     <>
-      <div className="container flex flex-col items-center justify-center h-screen bg-slate-100">
+      <div className="container flex flex-col items-center justify-center h-screen gap-5 bg-slate-100">
         <div>
           <h1>25 + 5 Clock</h1>
         </div>
         <div className="flex flex-row gap-5">
           <div>
             <div id="break-label">Break length</div>
-            <div>
-              <FaArrowUp /> 5 DOWN
+            <div className="flex items-center gap-2">
+              <FaArrowUp id="break-decrement" /> 5{' '}
+              <FaArrowDown id="break-increment" />
             </div>
           </div>
           <div>
             <div id="session-label">Session Length</div>
-            <div>UP 25 DOWN</div>
+            <div className="flex items-center gap-2">
+              <FaArrowUp id="session-decrement" /> 25{' '}
+              <FaArrowDown id="session-increment" />
+            </div>
           </div>
         </div>
+        <div></div>
       </div>
     </>
   );
