ea0bcc949f6556fa98cb64632d1df3de76536664
Said <slaoualy@yahoo.fr>
2023-09-10 21:13:25 +0200

    installed react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import { FaArrowUp } from 'react-icons/fa';
 import './App.css';
 
 function App() {
   return (
     <>
-      <div className="container h-screen bg-slate-100 flex flex-col items-center justify-center">
+      <div className="container flex flex-col items-center justify-center h-screen bg-slate-100">
         <div>
           <h1>25 + 5 Clock</h1>
         </div>
         <div className="flex flex-row gap-5">
           <div>
             <div id="break-label">Break length</div>
-            <div>UP 5 DOWN</div>
+            <div>
+              <FaArrowUp /> 5 DOWN
+            </div>
           </div>
           <div>
             <div id="session-label">Session Length</div>
