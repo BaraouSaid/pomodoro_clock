b14371b0096d3708aaa043c548aceeced8c92b03
Said <slaoualy@yahoo.fr>
2023-10-02 15:26:22 +0200

    created a new branch to format time correctly

--- src/App.jsx
+++ src/App.jsx
@@ -33,31 +33,19 @@ function decrementSessionLength(prev) {
   return prev;
 }
 
-function formatTime(sessionMinutes, sessionSeconds) {
-  // return `${minutes} < 10 ? "0" + ${minutes}:${minutes}:${seconds} < 10 ? "0" + ${seconds}`;
-  if (sessionMinutes < 10 && sessionSeconds < 10) {
-    return `0${sessionMinutes}:0${sessionSeconds}`;
-  }
-  if (sessionMinutes < 10) {
-    return `0${sessionMinutes}:${sessionSeconds}`;
-  }
-  if (sessionSeconds < 10) {
-    return `${sessionMinutes}:0${sessionSeconds}`;
-  }
-  return `${sessionMinutes}:${sessionSeconds}`;
-}
-
 function App() {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
-  const [sessionMinutes, setSessionMinutes] = useState(25);
-  const [sessionSeconds, setSessionSeconds] = useState(0);
-  const [timeLeft, setTimeLeft] = useState(
-    `${sessionMinutes}:0${sessionSeconds}`
-  );
+  const [timeLeft, setTimeLeft] = useState(1500);
+  const [sessionMinutes, setSessionMinutes] = useState(null);
+  const [sessionSeconds, setSessionSeconds] = useState(null);
   const [isCounting, setIsCounting] = useState(false);
   const [sessionTitle, setSessionTitle] = useState('Session');
   const [isOnBreak, setIsOnBreak] = useState(false);
+
+  function formatTime() {
+    return null;
+  }
 
   function reset() {
     setIsCounting(false);
@@ -65,8 +53,21 @@ function App() {
     setSessionLength(25);
     setSessionMinutes(25);
     setSessionSeconds(0);
-    // setTimeLeft(formatTime(25, 0));
   }
+
+  // function handleCount() {
+  //   clearTimeout(counter);
+  //   setIsCounting(!isCounting);
+  // }
+
+  // function countDown() {
+  //   if (isCounting) {
+  //     counter;
+  //     reset();
+  //   } else {
+  //     clearTimeout(counter);
+  //   }
+  // }
 
   useEffect(() => {
     if (isCounting) {
@@ -83,6 +84,9 @@ function App() {
           setIsOnBreak(true);
           setSessionMinutes(breakLength - 1);
           setSessionTitle('Break');
+        }
+        if (timeLeft == 0 && isOnBreak == false) {
+          setIsOnBreak(true);
         }
         if (sessionMinutes == 0 && sessionSeconds == 0 && isOnBreak == true) {
           setIsOnBreak(false);
@@ -108,17 +112,11 @@ function App() {
               <FaArrowUp
                 id="break-increment"
                 className="text-yellow-600 hover:cursor-pointer"
-                onClick={() =>
-                  setBreakLength(incrementBreakLength(breakLength))
-                }
               />
               <p id="break-length">{breakLength}</p>
               <FaArrowDown
                 id="break-decrement"
                 className="text-yellow-600 hover:cursor-pointer"
-                onClick={() =>
-                  setBreakLength(decrementBreakLength(breakLength))
-                }
               />
             </div>
           </div>
@@ -128,19 +126,11 @@ function App() {
               <FaArrowUp
                 id="session-increment"
                 className="text-amber-600 hover:cursor-pointer"
-                onClick={() => {
-                  setSessionLength(incrementSessionLength(sessionLength));
-                  setSessionMinutes(incrementSessionLength(sessionMinutes));
-                }}
               />
               <p id="session-length">{sessionLength}</p>
               <FaArrowDown
                 id="session-decrement"
                 className="text-amber-600 hover:cursor-pointer"
-                onClick={() => {
-                  setSessionLength(decrementSessionLength(sessionLength));
-                  setSessionMinutes(decrementSessionLength(sessionMinutes));
-                }}
               />
             </div>
           </div>
@@ -150,7 +140,7 @@ function App() {
             {sessionTitle}
           </h2>
           <p id="time-left" className="text-8xl">
-            {formatTime(sessionMinutes, sessionSeconds)}
+            MM:SS
           </p>
         </div>
         <div className="flex items-center gap-3">
